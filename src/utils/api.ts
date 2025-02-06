7dab610551972e988cb2d422d9f53c19e52d05a5
massaracsh7 <fatum7@gmail.com>
2025-02-06 10:58:40 +0300

    feat: add constants, api utils

---
+++ src/utils/api.ts
@@ -0,0 +1,23 @@
+import { API_URL } from '../constants/constants'
+import { ApiResponse, Character } from '../types/types'
+
+export async function searchCharacters (query: string) {
+  try {
+    const res: Response = await fetch(`${API_URL}/?name=${query}`)
+    const data: ApiResponse = await res.json()
+    console.log(data)
+    return data
+  } catch {
+    return null
+  }
+}
+
+export async function getCharacter (id: number) {
+  try {
+    const res: Response = await fetch(`${API_URL}/${id}`)
+    const data: Character = await res.json()
+    return data
+  } catch {
+    return null
+  }
+}
