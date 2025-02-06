6a30397c4910eee173b31196766e9be7da04bd5d
massaracsh7 <fatum7@gmail.com>
2025-02-06 12:47:36 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DetailPage from "./pages/DetailPage/DetailPage";
 import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
   return (
     <div className="app">
       <div className="container">
-        <MainPage />
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/character/:id" element={<DetailPage />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { CharacterArray } from "../../types/types";
 import styles from "./CardsList.module.css";
 
@@ -12,11 +13,13 @@ const CardsList: React.FC<CardsListProps> = ({ characters }) => {
         <ul className={styles.cardsList}>
           {characters.map((character) => (
             <li key={character.id} className={styles.cardsItem}>
+              <Link to={`/character/${character.id}`}>
               <h3>{character.name}</h3>
               <div>
                 <p>{character.status}</p>
                 <p>{character.created}</p>
               </div>
+              </Link>
             </li>
           ))}
         </ul>
