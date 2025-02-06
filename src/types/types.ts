fc175c3a95649f35dd39a1088994785a46449dc2
massaracsh7 <fatum7@gmail.com>
2025-02-06 10:52:44 +0300

    feat: add types

---
+++ src/types/types.ts
@@ -0,0 +1,37 @@
+export type PageNumber = string | null
+export type Gender = 'unknown' | 'Female' | 'Male' | 'Genderless'
+export type Status = 'Dead' | 'Alive' | 'unknown'
+
+export interface Info {
+  count: number
+  pages: number
+  next: PageNumber
+  prev: PageNumber
+}
+
+export interface CharacterLocation {
+  name: string
+  url: string
+}
+
+export interface Character {
+  id: number
+  name: string
+  status: Status
+  species: string
+  type: string
+  gender: Gender
+  origin: CharacterLocation
+  location: CharacterLocation
+  image: string
+  episode: string[]
+  url: string
+  created: string
+}
+
+export type CharacterArray = Array<Character> | false
+
+export interface ApiResponse {
+  info: Info
+  results: CharacterArray
+}
