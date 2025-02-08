de79ac5d94d4b3805523bf497040fdc64ac57ffc
massaracsh7 <fatum7@gmail.com>
2025-02-08 23:00:02 +0300

    refactor: update input search

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -8,12 +8,12 @@ interface InputSearchProps {
 
 const InputSearch: React.FC<InputSearchProps> = ({ setSearch, search }) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
-
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!/^[a-z\s]*$/i.test(e.target.value)) return;
     setSearch(e.target.value.toLowerCase().trim());
   };
 
